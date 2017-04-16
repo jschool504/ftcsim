1a024cd415f27916badc71a044525ebded9c736a
Jeremy School <jschool504@gmail.com>
2017-04-16 17:16:16 -0400

    changed sample package to ftcim

--- src/ftcsim/Controller.java
+++ src/ftcsim/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package ftcsim;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
--- src/ftcsim/Main.java
+++ src/ftcsim/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package ftcsim;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
